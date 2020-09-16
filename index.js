8d9cfed348b5bf567b5de731432c027c2b230f18
Rick Groenewegen <west14@gmail.com>
2020-09-16 11:16:07 +0200

    New functions

--- index.js
+++ index.js
@@ -29,17 +29,42 @@ var functionMappings = {
 	}
 };
 
-var FastDate = function(obj) {
+var formatFunctions = {
+	'default' : function(obj) {
+		return obj.getFullYear() + '-' + FastDate.pad(obj.getMonth() + 1,2) + '-' + FastDate.pad(obj.getDate(),2) + 'T' + FastDate.pad(obj.getHours(),2) + ':' + FastDate.pad(obj.getMinutes(),2) + ':' + FastDate.pad(obj.getSeconds(),2) + formatUTCOffset(obj);
+	}
+};
+
+var parseMappings = {};
+
+var FastDate = function(obj,format) {
 
   if (!(this instanceof FastDate)) {
-    return new FastDate(obj);
+    return new FastDate(obj,format);
+  } else if(obj instanceof FastDate) {
+  	return obj;
+  } else {
+  	if(!obj) {
+	  	obj = new Date();
+	  } else if (format) {
+		obj = fns.parse(obj,convertFormat(format),new Date());
+	  } else {
+	  	obj = new Date(obj);
+	  }
   }
-
-  if(!obj) {
-  	obj = new Date();
-  }
+  
 
   this.obj = obj;
+};
+
+var convertFormat = function(format) {
+	var newFormat = format;
+	if(parseMappings[format]) {
+		newFormat = parseMappings[format];
+	} else {
+		throw 'Invalid parse format: ' + format;
+	}
+	return newFormat;
 };
 
 var formatUTCOffset = function(obj) {
@@ -48,11 +73,7 @@ var formatUTCOffset = function(obj) {
 	return dif + FastDate.pad(tzo / 60,2) + ':' + FastDate.pad(tzo % 60,2);
 };
 
-var formatFunctions = {
-	'default' : function(obj) {
-		return obj.getFullYear() + '-' + FastDate.pad(obj.getMonth() + 1,2) + '-' + FastDate.pad(obj.getDate(),2) + 'T' + FastDate.pad(obj.getHours(),2) + ':' + FastDate.pad(obj.getMinutes(),2) + ':' + FastDate.pad(obj.getSeconds(),2) + formatUTCOffset(obj);
-	}
-};
+FastDate.formatUTCOffset = formatUTCOffset;
 
 FastDate.pad = function(num, size) {
     var s = num + '';
@@ -62,6 +83,10 @@ FastDate.pad = function(num, size) {
 
 FastDate.addFormatFunction = function format(format,func) {
 	formatFunctions[format] = func;
+};
+
+FastDate.addParseMapping = function format(format,val) {
+	parseMappings[format] = val;
 };
 
 FastDate.prototype.format = function format(format) {
@@ -74,14 +99,18 @@ FastDate.prototype.format = function format(format) {
 	} else {
 		throw 'Unsupported format: ' + format;
 	}
-  	return returnVal;
+	return returnVal;
 };
 
 
 FastDate.prototype.diff = function format(date2,datepart) {
 	var returnValue = null;
+	var compareDate = date2;
+	if(date2 instanceof FastDate) {
+		compareDate = date2.obj;
+	}
 	try {
-		returnValue = functionMappings.diff[datepart](this.obj, date2);
+		returnValue = functionMappings.diff[datepart](this.obj, compareDate);
 	} catch(e) {
 		throw 'Invalid datepart: ' + datepart;
 	}
@@ -97,6 +126,15 @@ FastDate.prototype.add = function format(amount,datepart) {
 	return this;	
 };
 
+FastDate.prototype.subtract = function format(amount,datepart) {
+	try {
+		this.obj = functionMappings.add[datepart](this.obj, -amount);
+	} catch(e) {
+		throw 'Invalid datepart: ' + datepart;
+	}
+	return this;	
+};
+
 FastDate.prototype.set = function format(params) {
 	for (var param in params) {
 		if(param == 'year') {
@@ -105,19 +143,31 @@ FastDate.prototype.set = function format(params) {
 			this.obj.setMonth(params[param]);
 		} else if(param == 'date') {
 			this.obj.setDate(params[param]);
-		} else if(param == 'hour') {
+		} else if(param == 'hour' || param == 'hours') {
 			this.obj.setHours(params[param]);
-		} else if(param == 'minute') {
+		} else if(param == 'minute' || param == 'minutes') {
 			this.obj.setMinutes(params[param]);
-		} else if(param == 'second') {
+		} else if(param == 'second' || param == 'seconds') {
 			this.obj.setSeconds(params[param]);
-		} else if(param == 'millisecond') {
+		} else if(param == 'millisecond' || param == 'milliseconds') {
 			this.obj.setMilliseconds(params[param]);
 		} else {
 			throw 'Invalid datepart' + param;
 		}
 	}
 	return this;	
+};
+
+FastDate.prototype.toDate = function format(amount,datepart) {
+	return this.obj;	
+};
+
+FastDate.prototype.utcOffset = function format(amount,datepart) {
+	return -this.obj.getTimezoneOffset();
+};
+
+FastDate.prototype.day = function format(amount,datepart) {
+	return this.obj.getDay();;	
 };
 
 module.exports = FastDate;
